64cd554ae3fe54dc8a65795206c6a590e4945e87
בר קזז <barkazzaz@gmail.com>
2018-10-26 13:15:12 +0300

    merged Dima and Bar to one project.

--- MoodTube/Controllers/HomeController.cs
+++ MoodTube/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MoodTube.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Home");
         }
 
         public IActionResult About()
